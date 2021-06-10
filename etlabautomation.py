b03c5258a8067eb66648d107a29abe4c1fb75c45
Abish Vijayan <abishvijayan7643@gmail.com>
2021-06-10 16:50:24 +0530

    Timeout Handling in Subjects and Questions Page, Logout Method

--- etlabautomation.py
+++ etlabautomation.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from selenium.common import exceptions
 import time
 
 
@@ -12,7 +16,11 @@ class EtlabBot:
         self.password = password
         self.bot = webdriver.Firefox()
 
-    def login(self, userLoginLink):
+    def login(self, userLoginLink=None):
+
+        if userLoginLink is None:
+            userLoginLink = "https://sctce.etlab.in/user/login"
+
         bot = self.bot
         bot.get(userLoginLink)
         print("------------ Delay of 5s -----------")
@@ -37,42 +45,88 @@ class EtlabBot:
 
         rows = bot.find_elements_by_id("section-form")
         totalSubjects = len(rows)
-        print('----- {} Subjects To Be Done ------'.format(totalSubjects))
+        print('------ {} Subjects To Be Done -------'.format(totalSubjects))
 
         totalQuestions = len(AnswerValueHTML)
         print('----- {} Questions To Be Done ------'.format(totalQuestions))
 
         # Iterating Through Each Subject
-        for i in range(totalSubjects):
-            print("------------ Delay of {}s -----------".format(delay))
-            print('------------ Subject {} ------------'.format(i + 1))  # Range Means From 0 to i-1, So Making it 1 + 0
-            currentSubjectName = 'yt{}'.format(i)  # Selects The Button "Answer Questions"
-            currentSubjectSelector = bot.find_element_by_name(currentSubjectName)
-            currentSubjectSelector.click()
-            print("------------ Delay of {}s -----------".format(delay))  # wait till answer loads
-            time.sleep(delay)
+        subjectIterator = 0
+        while subjectIterator < len(range(totalSubjects)):
+            print('============= Subject {} ============'.format(subjectIterator + 1))
 
-            questionNumber = 1
-            # Iterating Through Each Question
-            for j in AnswerValueHTML:
-                print('------------ Question {} ------------'.format(questionNumber))
-                answerSelector = bot.find_element_by_xpath(
-                    ".//input[@value='{}']".format(j))  # Selecting Required Answer
-                answerSelector.click()
-                questionNumber += 1
+            # Selects The Button "Answer Questions"
+            currentSubjectName = 'yt{}'.format(subjectIterator)
 
-            print("------- Selected Req Answers -------")
-            print("------------ Delay of {}s -----------".format(delay))  # Wait to Verify
-            time.sleep(delay)
+            # Click On Subject
+            subjectTimeout = False
+            try:
+                currentSubjectSelector = WebDriverWait(bot, 15).until(
+                    expected_conditions.presence_of_element_located((By.NAME, currentSubjectName))
+                )
+                currentSubjectSelector.click()
+            except exceptions.TimeoutException:
+                print("-------------- Timeout --------------")
+                subjectTimeout = True
 
-            submitButtonName = 'yt0'  # Name Attribute Of Submit Button From HTML
-            submitButton = bot.find_elements_by_name(submitButtonName)
-            submitButton[0].click()
+            if subjectTimeout:
+                print("-------------- Redoing --------------")
+            else:
+                # wait till answer loads
+                print("------------ Delay of {}s -----------".format(delay))
+                time.sleep(delay)
 
-            print("------------ Delay of {}s -----------".format(delay))
-            time.sleep(delay)
+                # Iterating Through Each Question
+                questionIterator = 0
+                while questionIterator < len(AnswerValueHTML):
+                    print('------------ Question {} ------------'.format(questionIterator + 1))
+
+                    # Click on Answer
+                    questionTimeout = False
+                    try:
+                        answerSelector = WebDriverWait(bot, 15).until(
+                            expected_conditions.presence_of_element_located((
+                                By.XPATH, ".//input[@value='{}']".format(AnswerValueHTML[questionIterator]))
+                            )
+                        )
+                        answerSelector.click()
+                    except exceptions.TimeoutException:
+                        print("-------------- Timeout --------------")
+                        questionTimeout = True
+
+                    if questionTimeout is not True:
+                        questionIterator += 1
+                    else:
+                        print("-------------- Redoing --------------")
+
+                print("------- Selected Req Answers -------")
+
+                # Wait to Verify
+                print("------------ Delay of {}s -----------".format(delay))
+                time.sleep(delay)
+
+                # Name Attribute Of Submit Button From HTML
+                submitButtonName = 'yt0'
+                submitButton = bot.find_elements_by_name(submitButtonName)
+                submitButton[0].click()
+
+                # Next Subject
+                subjectIterator += 1
+
+                print("------------ Delay of {}s -----------".format(delay))
+                time.sleep(delay)
 
         print("------------------------------------")
         print("--------- Survey Completed ---------")
         print("------- Phew! That Was Easy! -------")
         print("------------------------------------")
+
+    def logout(self, logoutLink=None):
+
+        if logoutLink is None:
+            logoutLink = "https://sctce.etlab.in/user/logout"
+
+        bot = self.bot
+        bot.get(logoutLink)
+        print("------------ Logged Out ------------")
+        bot.quit()
--- etlabsurvey.py
+++ etlabsurvey.py
@@ -5,18 +5,19 @@ from etlabautomation import EtlabBot
 # User Credentials
 username = 'username'
 password = 'password'
-loginlink = 'https://sctce.etlab.in/user/login'
-
-# Survey
 surveylink = 'https://sctce.etlab.in/survey/user/answer/626'
 
 # Required Answer To Be Filled (Get Answer Value From HTML)
-# answerReference = [5727, 5729, 5731, 5733, 5737, 5741, 5743, 5746, 5749]  # Semester8
-answerReference = [450, 455, 460, 465, 469, 472, 476, 479, 483, 487, 491] # Semester7/Semester8
+# answerReference = [5727, 5729, 5731, 5733, 5737, 5741, 5743, 5746, 5749]
+answerReference = [450, 455, 460, 465, 469, 472, 476, 479, 483, 487, 491]
 
 # Bot
 delay = 2
-SurveyBot = EtlabBot(username, password)  # Create an Object & Pass Your Username and Password
-SurveyBot.login(loginlink)  # Pass The Login Link
-SurveyBot.doTeacherEvaluationSurveys(surveylink, answerReference,
-                                     delay)  # Add The Survey Link & Add the Number Of Subjects to Be Filled
+# Create an Object & Pass Your Username and Password
+SurveyBot = EtlabBot(username, password)
+# Pass The Login Link
+SurveyBot.login("https://sctce.etlab.in/user/login")
+# Add The Survey Link & Add the Number Of Subjects to Be Filled
+SurveyBot.doTeacherEvaluationSurveys(surveylink, answerReference, delay)
+# Logout
+SurveyBot.logout("https://sctce.etlab.in/user/logout")
